61ddd8dc2c13d5447a4a34412d06c2f5d7ec8f2c
MasterToninus <antoniomichele.miti@gmail.com>
2019-01-09 16:45:12 +0100

    Updated Csv Parser

--- data/activities_csv_parser.py
+++ data/activities_csv_parser.py
@@ -6,52 +6,100 @@
 #   http://2017.compciv.org/guide/topics/python-standard-library/csv.html
 
 # File di output
-filelatex = open('Latex_activities.txt', 'w')
+filelatex = open('Latex_activities.tex', 'w')
+filelatex.write('\\documentclass{article}\n\\usepackage{hyperref}\n\\begin{document}')
 #filehtml = open('Html_attivita.txt', 'w')
 DEBUG = 0
 
 
-# Funzione Record to Latex
-def recordtolatex( record ):
+# Funzione List of Record to Latex
+def listrecordtolatex( listrecord ):
 	"DocString"
-	filelatex.write("\item\n")
-	filelatex.write("\t%s: \href{%s}\n\t{\emph{%s}},\n" % (row["Type"],row['Url'],row['Event_Title']))
-	filelatex.write("\t(Prof: %s),\n" % (row["Organizers_Speakers"]))
-	filelatex.write("\t%s, %s, %s, %s;\n\n" % (row['Approx_Date'],row['Organization'],row['Location'],row['Country']))
+	line_count = 0
+	filelatex.write("\n\n \\begin{itemize}\n")
+	for row in listrecord:
+		line_count += 1
+		#if row['Period']=="II Year":
+		filelatex.write("\item\n")
+		filelatex.write("\t%s: \href{%s}\n\t{\emph{%s}},\n" % (row["Type"],row['Url'],row['Event_Title']))
+		filelatex.write("\t(Prof: %s),\n" % (row["Organizers_Speakers"]))
+		filelatex.write("\t%s, %s, %s, %s;\n\n" % (row['Approx_Date'],row['Organization'],row['Location'],row['Country']))
+	filelatex.write("\end{itemize}\n")
+	print(line_count)
 	return
 
-def recordtolatex_noprof( record ):
+def listrecordtolatex_noprof( record ):
 	"DocString"
-	filelatex.write("\item\n")
-	filelatex.write("\t%s: \href{%s}\n\t{\emph{%s}},\n" % (row["Type"],row['Url'],row['Event_Title']))
-	#filelatex.write("\t(Prof: %s),\n" % (row["Organizers_Speakers"]))
-	filelatex.write("\t%s, %s, %s, %s;\n\n" % (row['Approx_Date'],row['Organization'],row['Location'],row['Country']))
+	line_count = 0
+	filelatex.write("\n\n\\begin{itemize}\n")
+	for row in listrecord:
+		line_count += 1
+		#if row['Period']=="II Year":
+		filelatex.write("\item\n")
+		filelatex.write("\t%s: \href{%s}\n\t{\emph{%s}},\n" % (row["Type"],row['Url'],row['Event_Title']))
+		#filelatex.write("\t(Prof: %s),\n" % (row["Organizers_Speakers"]))
+		filelatex.write("\t%s, %s, %s, %s;\n\n" % (row['Approx_Date'],row['Organization'],row['Location'],row['Country']))
+	filelatex.write("\end{itemize}\n")
+	print(line_count)
 	return
 
+def checkworkshop( str ):
+	return str == 'Workshop' or str == 'Conference'
 
+def checkcourses( str ):
+	return str == 'Ph.D. Course' or str == 'Master Course'
+
+def checkcommunications( str ):
+	return str == 'Workshop' or str == 'Conference'
+
+def checkothers( str ):
+	return str == 'Workshop' or str == 'Conference'
+
+conferences_tuple = ('Workshop', 'Conference')
+courses_tuple = ('Ph.D. Course', 'Master Course', 'School', 'Training Course')
+talks_tuple = ('Invited Talk', 'Contributed Talk', 'Poster')
+others_tuple = ('Teaching', 'Certificate', 'Preprint', 'Publication', 'Other', 'Type', 'Seminar')
+
+from datetime import datetime
+def antichronosort(list_to_be_sorted):
+	return sorted(list_to_be_sorted, key=lambda k: datetime.strptime( k['Start_Date'], '%d-%b-%y'), reverse=True)
 
 
 import csv
 with open('activities.csv', newline='') as csvfile:
-    csv_reader = csv.DictReader(csvfile, delimiter=';', quotechar='|')
+	csv_reader = csv.DictReader(csvfile, delimiter=';', quotechar='|')
 
-    #Deserializzo il fileReader in una lista di records
-    records = list(csv_reader)
-    if(DEBUG==1):
-	    print(type(records))
-	    print(type(records[1]))
-	    print(records[1])
+	#Deserializzo il fileReader in una lista di records (dictionaries)
+	records = list(csv_reader)
 
-    #Nota: se non avessi scorso tutto lo stream a dare la lista avrei potuto ciclare direttamente su filereader
-    line_count = 0
-    for row in records:
-        if line_count == 0:
-            print("HEADER: "+", ".join(row))
-            line_count += 1
-        #if row['Period']=="II Year":
-        recordtolatex(row)
-        line_count += 1
-    print("Processed %d lines" %line_count )
+print("HEADER: "+", ".join(records[0])+"\n")
 
+workshops= antichronosort(list(filter(lambda row: row['Type'] in conferences_tuple,records)))
+courses= antichronosort(list(filter(lambda row: row['Type'] in courses_tuple,records)))
+talks= antichronosort(list(filter(lambda row: row['Type'] in talks_tuple,records)))
+others= antichronosort(list(filter(lambda row: row['Type'] in others_tuple,records)))
+
+#CHECK: missed items
+print('missing item ?')
+print([item['Type'] for item in records if item not in workshops+courses+talks+others])
+
+
+print('\n Workshops')
+filelatex.write('\n\\section{workshop}\n')
+listrecordtolatex(workshops)
+
+print('\n courses')
+filelatex.write('\n\\section{courses}\n')
+listrecordtolatex(courses)
+
+print('\n talks')
+filelatex.write('\n\\section{talks}\n')
+listrecordtolatex(talks)
+
+print('\n others')
+filelatex.write('\n\\section{others}\n')
+listrecordtolatex(others)
+
+filelatex.write('\\end{document}')
 filelatex.close()
 #filehtml.close()

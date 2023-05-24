083e7394e76a9f6e0b89f2e97c5f3bda367ac20b
MasterToninus <antoniomichele.miti@gmail.com>
2023-05-24 19:00:12 +0200

    May Updates

--- gunter/facciata.php
+++ gunter/facciata.php
@@ -123,7 +123,7 @@
     <!-- ================================================= -->
     <!-- ??? -->
     <!-- ================================================= -->            
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
     <!-- Github stars script -->

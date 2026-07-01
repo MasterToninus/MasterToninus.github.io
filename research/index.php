e4b4a204991ab676e4a8290b4b947557386b7612
MasterToninus <antoniomichele.miti@gmail.com>
2026-07-01 16:05:30 +0200

    Tiny changes in link to bibliometrics page.

--- research/index.php
+++ research/index.php
@@ -196,7 +196,7 @@
           <div class="sec-small">
             <table class="center" style="width: 80%;text-align: center">
              <tr>
-              <td>(<a href="../bibliometrics/" class="btn btn-default btn-lg"><i class="fa fa-tachometer fa-fw"></i><span class="network-name"> Bibliometrics - dedicated page</span></a> )
+              <td>(<a href="../bibliometrics/" class="btn btn-default btn-lg"><i class="fa fa-tachometer fa-fw"></i><span class="network-name"> Bibliometrics <i class="fa fa-tachometer fa-fw"></i><span class="network-name"></span></a> )
             </td>
 		        </table>
             <br>            
